21d4f8ad02ffde9720871673d23a54ff95cee276
mohammaduzzal <aalp72142@gmail.com>
2025-06-20 22:26:45 +0600

    added single book update delete method

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -5,8 +5,8 @@ const createBook = async (req: Request, res: Response) => {
     try {
         const data = await Book.create(req.body)
         res.status(201).send({
-            success : true,
-            message : "book created successfully",
+            success: true,
+            message: "book created successfully",
             data
         })
 
@@ -18,8 +18,102 @@ const createBook = async (req: Request, res: Response) => {
         });
 
     }
-}
+};
+
+const getBooks = async (req: Request, res: Response) => {
+    try {
+        // const data = await Book.find();
+        const data = await Book.find().sort({"createdAt" : "asc"}).limit(10);
+        res.status(200).send({
+            success: true,
+            message: "Books retrieved successfully",
+            data
+        })
+
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+
+const getBookById = async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId;
+        const data = await Book.findById(bookId);
+        if(!data){
+            return res.status(404).send({
+                success :false,
+                message:"Book not found"
+            })
+        }
+        res.status(200).send({
+            success: true,
+            message: "book retrieved successfully",
+            data
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+
+    }
+
+};
+
+const updateBook = async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId;
+        const data = await Book.findByIdAndUpdate(bookId, req.body, {
+            new: true,
+            runValidators: true
+        })
+        res.status(200).send({
+            success: true,
+            message: "Book updated successfully",
+            data
+        })
+
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
+};
+
+const deleteBook = async(req: Request, res: Response) =>{
+    try {
+        const bookId = req.params.bookId;
+        const data = await Book.findByIdAndDelete(bookId);
+         res.status(200).send({
+            success : true,
+            message : "Book deleted successfully",
+            data
+        })
+        
+    } catch (error) {
+         res.status(500).send({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+        
+    }
+};
 
 export const bookController = {
-    createBook
-}
+    createBook,
+    getBooks,
+    getBookById,
+    updateBook,
+    deleteBook
+};
--- src/modules/book/book.route.ts
+++ src/modules/book/book.route.ts
@@ -4,5 +4,9 @@ import { bookController } from "./book.controller";
 const bookRoute = Router();
 
 bookRoute.post('/', bookController.createBook)
+bookRoute.get('/:bookId', bookController.getBookById)
+bookRoute.patch('/:bookId', bookController.updateBook)
+bookRoute.delete('/:bookId', bookController.deleteBook)
+bookRoute.get('/', bookController.getBooks)
 
 export default bookRoute;
