659b057479d9bc26c64cb83f63758421a64845e2
mohammaduzzal <aalp72142@gmail.com>
2025-06-20 16:14:35 +0600

    basic setup for project

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
+import route from './modules/routes';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(route);
+
+
+
+app.get("/", (req,res)=>{
+    res.send({
+        success :true,
+        message : "hey it is library management server"
+    })
+});
+
+app.listen(config.port, ()=>{
+    console.log(`✔ express server is running on server ${config.port}`);
+})
+
+// connect to mongodb
+async function server() {
+    try {
+        await mongoose.connect(config.database_url as string)
+        console.log(`🍃 MongoDB connected successfully`);
+    } catch (error) {
+          console.error(`❌ MongoDB connection error:`, error);
+    }
+}
+
+server()
